8f91dc5f37d9e8025b68043f1c311e6c7e816eb4
KingTeslaw <ehsan.nasiri1384@gmail.com>
2025-10-23 19:21:44 +0330

    RTL Theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,35 +4,38 @@ import { useState } from "react";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between bg-dark-bg text-white px-6 py-3 ">
-      {/* Left side (Logo + Menu) */}
-      <div className="flex items-center space-x-4">
-        <h1 className="text-xl font-semibold tracking-wide">Dashboard</h1>
-        <nav className="hidden md:flex space-x-6 text-slate-300">
-          <a href="#" className="hover:text-white">
-            Pages
-          </a>
-          <a href="#" className="hover:text-white">
-            Posts
-          </a>
-        </nav>
-        {/* Divider */}
-        <div className="h-4 w-px bg-slate-700"></div>
-        {/* Center (Search Bar) */}
-        <div className="hidden md:flex  ">
-          <FaSearch className="text-slate-400 mr-2" />
+    <header className="w-[1820px] bg-dark-bg text-white">
+      <div className="container flex items-center justify-between px-6 py-3 ">
+        {/* Right side (Logo + Menu) */}
+        <div className="flex items-center">
+          <nav className="hidden md:flex  text-slate-300 gap-x-10">
+            <a href="#" className="hover:text-white">
+              داشبورد
+            </a>
+            <a href="#" className="hover:text-white">
+              صفحات
+            </a>
+            <a href="#" className="hover:text-white">
+              پست ها
+            </a>
+          </nav>
+          {/* Divider */}
+          <div className="h-4 w-px bg-slate-700 mr-10 "></div>
+          {/* Center (Search Bar) */}
+          <div className="hidden md:flex mr-10 cursor-pointer">
+            <FaSearch className="text-slate-400 mr-2" />
+          </div>
         </div>
-      </div>
-
-      {/* Right side (Icons & User) */}
-      <div className="flex items-center space-x-4">
-        <div className="flex items-center space-x-2 cursor-pointer">
-          <FaUserCircle size={28} className="text-blue-400" />
-          <span className="hidden md:inline text-sm font-medium">
-            UX Designer
-          </span>
+        {/* Left side (Icons & User) */}
+        <div className="flex items-center gap-x-4">
+          <FaBell className="text-slate-400 hover:text-white cursor-pointer" />
+          <div className="flex items-center gap-x-2 cursor-pointer">
+            <span className="hidden md:inline text-md font-medium">
+              دیزاینر سایت
+            </span>
+            <FaUserCircle size={28} className="text-blue-400" />
+          </div>
         </div>
-        <FaBell className="text-slate-400 hover:text-white cursor-pointer" />
       </div>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,27 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemiBold: "Dana DemiBold",
+      },
       colors: {
         "dark-bg": "#131B2F",
         "darker-bg": "#071026",
       },
+      container: {
+        center: true,
+        screens: {
+          DEFAULT: "1775px",
+        },
+      },
     },
+    plugins: [
+      function ({ addVariant }) {
+        addVariant("child", "& > * ");
+        addVariant("child-hover", "& > *:hover");
+      },
+    ],
   },
-  plugins: [],
 };
