ac6080ee95d8bc46088b0467fe1d542c161975e6
KingTeslaw <ehsan.nasiri1384@gmail.com>
2025-10-28 23:48:37 +0330

    Users Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "./components/Home/Home";
+import Users from "./components/Users/Users";
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
         {/* اگر مسیر وجود نداشت صفحه 404 نمایش داده شود */}
-        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
